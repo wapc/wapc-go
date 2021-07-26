3b6de54cfc490f8156aeb20e00bb972181ff426b
Phil Kedy <phil.kedy@gmail.com>
2021-07-25 21:13:43 -0400

    Simplification

--- module.go
+++ module.go
@@ -9,8 +9,6 @@ import (
 	"github.com/wasmerio/wasmer-go/wasmer"
 )
 
-const initialNumFunctions = 20
-
 type (
 	// Logger is the function to call from consoleLog inside a waPC module.
 	Logger func(msg string)
@@ -38,9 +36,20 @@ type (
 		inst *wasmer.Instance
 		mem  *wasmer.Memory
 
-		context    *invokeContext
-		_functions [initialNumFunctions]wasmer.IntoExtern
-		functions  []wasmer.IntoExtern
+		context *invokeContext
+
+		// external functions
+		guestRequest    *wasmer.Function
+		guestResponse   *wasmer.Function
+		guestError      *wasmer.Function
+		hostCall        *wasmer.Function
+		hostResponseLen *wasmer.Function
+		hostResponse    *wasmer.Function
+		hostErrorLen    *wasmer.Function
+		hostError       *wasmer.Function
+		consoleLog      *wasmer.Function
+		abort           *wasmer.Function
+		fdWrite         *wasmer.Function
 	}
 
 	invokeContext struct {
@@ -94,11 +103,10 @@ func (m *Module) Instantiate() (*Instance, error) {
 	instance := Instance{
 		m: m,
 	}
-	instance.functions = instance._functions[:0]
 	importObject := wasmer.NewImportObject()
-	importObject.Register("env", instance.addFunctions(envRuntime(m.store, &instance)))
-	importObject.Register("wapc", instance.addFunctions(wapcRuntime(m.store, &instance)))
-	wasiRuntime := instance.addFunctions(wasiRuntime(m.store, &instance))
+	importObject.Register("env", instance.envRuntime())
+	importObject.Register("wapc", instance.wapcRuntime())
+	wasiRuntime := instance.wasiRuntime()
 	importObject.Register("wasi_unstable", wasiRuntime)
 	importObject.Register("wasi_snapshot_preview1", wasiRuntime)
 	importObject.Register("wasi", wasiRuntime)
@@ -137,200 +145,204 @@ func (m *Module) Instantiate() (*Instance, error) {
 	return &instance, nil
 }
 
-func envRuntime(store *wasmer.Store, inst *Instance) map[string]wasmer.IntoExtern {
+func (i *Instance) envRuntime() map[string]wasmer.IntoExtern {
+	// abort is for assemblyscript
+	i.abort = wasmer.NewFunction(
+		i.m.store,
+		wasmer.NewFunctionType(wasmer.NewValueTypes(wasmer.I32, wasmer.I32, wasmer.I32, wasmer.I32), wasmer.NewValueTypes()),
+		func(args []wasmer.Value) ([]wasmer.Value, error) {
+			return []wasmer.Value{}, nil
+		},
+	)
 	return map[string]wasmer.IntoExtern{
-		// abort is for assemblyscript
-		"abort": wasmer.NewFunction(
-			store,
-			wasmer.NewFunctionType(wasmer.NewValueTypes(wasmer.I32, wasmer.I32, wasmer.I32, wasmer.I32), wasmer.NewValueTypes()),
-			func(args []wasmer.Value) ([]wasmer.Value, error) {
-				return []wasmer.Value{}, nil
-			},
-		),
+		"abort": i.abort,
 	}
 }
 
-func wapcRuntime(store *wasmer.Store, inst *Instance) map[string]wasmer.IntoExtern {
-	return map[string]wasmer.IntoExtern{
-		"__guest_request": wasmer.NewFunction(
-			store,
-			wasmer.NewFunctionType(wasmer.NewValueTypes(wasmer.I32, wasmer.I32), wasmer.NewValueTypes()),
-			func(args []wasmer.Value) ([]wasmer.Value, error) {
-				operationPtr := args[0].I32()
-				payloadPtr := args[1].I32()
-				data := inst.mem.Data()
-				copy(data[operationPtr:], inst.context.operation)
-				copy(data[payloadPtr:], inst.context.guestReq)
+func (i *Instance) wapcRuntime() map[string]wasmer.IntoExtern {
+	i.guestRequest = wasmer.NewFunction(
+		i.m.store,
+		wasmer.NewFunctionType(wasmer.NewValueTypes(wasmer.I32, wasmer.I32), wasmer.NewValueTypes()),
+		func(args []wasmer.Value) ([]wasmer.Value, error) {
+			operationPtr := args[0].I32()
+			payloadPtr := args[1].I32()
+			data := i.mem.Data()
+			copy(data[operationPtr:], i.context.operation)
+			copy(data[payloadPtr:], i.context.guestReq)
+			return []wasmer.Value{}, nil
+		},
+	)
+	i.guestResponse = wasmer.NewFunction(
+		i.m.store,
+		wasmer.NewFunctionType(wasmer.NewValueTypes(wasmer.I32, wasmer.I32), wasmer.NewValueTypes()),
+		func(args []wasmer.Value) ([]wasmer.Value, error) {
+			ptr := args[0].I32()
+			len := args[1].I32()
+			data := i.mem.Data()
+			buf := make([]byte, len)
+			copy(buf, data[ptr:ptr+len])
+			i.context.guestResp = buf
+			return []wasmer.Value{}, nil
+		},
+	)
+	i.guestError = wasmer.NewFunction(
+		i.m.store,
+		wasmer.NewFunctionType(wasmer.NewValueTypes(wasmer.I32, wasmer.I32), wasmer.NewValueTypes()),
+		func(args []wasmer.Value) ([]wasmer.Value, error) {
+			ptr := args[0].I32()
+			len := args[1].I32()
+			data := i.mem.Data()
+			cp := make([]byte, len)
+			copy(cp, data[ptr:ptr+len])
+			i.context.guestErr = string(cp)
+			return []wasmer.Value{}, nil
+		},
+	)
+	i.hostCall = wasmer.NewFunction(
+		i.m.store,
+		wasmer.NewFunctionType(wasmer.NewValueTypes(wasmer.I32, wasmer.I32, wasmer.I32, wasmer.I32, wasmer.I32, wasmer.I32, wasmer.I32, wasmer.I32), wasmer.NewValueTypes(wasmer.I32)),
+		func(args []wasmer.Value) ([]wasmer.Value, error) {
+			bindingPtr := args[0].I32()
+			bindingLen := args[1].I32()
+			namespacePtr := args[2].I32()
+			namespaceLen := args[3].I32()
+			operationPtr := args[4].I32()
+			operationLen := args[5].I32()
+			payloadPtr := args[6].I32()
+			payloadLen := args[7].I32()
+
+			if i.m.hostCallHandler == nil {
+				return []wasmer.Value{wasmer.NewI32(0)}, nil
+			}
+
+			data := i.mem.Data()
+			binding := string(data[bindingPtr : bindingPtr+bindingLen])
+			namespace := string(data[namespacePtr : namespacePtr+namespaceLen])
+			operation := string(data[operationPtr : operationPtr+operationLen])
+			payload := make([]byte, payloadLen)
+			copy(payload, data[payloadPtr:payloadPtr+payloadLen])
+
+			i.context.hostResp, i.context.hostErr = i.m.hostCallHandler(i.context.ctx, binding, namespace, operation, payload)
+			if i.context.hostErr != nil {
+				return []wasmer.Value{wasmer.NewI32(0)}, nil
+			}
+
+			return []wasmer.Value{wasmer.NewI32(1)}, nil
+		},
+	)
+	i.hostResponseLen = wasmer.NewFunction(
+		i.m.store,
+		wasmer.NewFunctionType(wasmer.NewValueTypes(), wasmer.NewValueTypes(wasmer.I32)),
+		func(args []wasmer.Value) ([]wasmer.Value, error) {
+			return []wasmer.Value{wasmer.NewI32(int32(len(i.context.hostResp)))}, nil
+		},
+	)
+	i.hostResponse = wasmer.NewFunction(
+		i.m.store,
+		wasmer.NewFunctionType(wasmer.NewValueTypes(wasmer.I32), wasmer.NewValueTypes()),
+		func(args []wasmer.Value) ([]wasmer.Value, error) {
+			if i.context.hostResp != nil {
+				ptr := args[0].I32()
+				data := i.mem.Data()
+				copy(data[ptr:], i.context.hostResp)
+			}
+			return []wasmer.Value{}, nil
+		},
+	)
+	i.hostErrorLen = wasmer.NewFunction(
+		i.m.store,
+		wasmer.NewFunctionType(wasmer.NewValueTypes(), wasmer.NewValueTypes(wasmer.I32)),
+		func(args []wasmer.Value) ([]wasmer.Value, error) {
+			if i.context.hostErr == nil {
+				return []wasmer.Value{wasmer.NewI32(0)}, nil
+			}
+			errStr := i.context.hostErr.Error()
+			return []wasmer.Value{wasmer.NewI32(int32(len(errStr)))}, nil
+		},
+	)
+	i.hostError = wasmer.NewFunction(
+		i.m.store,
+		wasmer.NewFunctionType(wasmer.NewValueTypes(wasmer.I32), wasmer.NewValueTypes()),
+		func(args []wasmer.Value) ([]wasmer.Value, error) {
+			if i.context.hostErr == nil {
 				return []wasmer.Value{}, nil
-			},
-		),
-		"__guest_response": wasmer.NewFunction(
-			store,
-			wasmer.NewFunctionType(wasmer.NewValueTypes(wasmer.I32, wasmer.I32), wasmer.NewValueTypes()),
-			func(args []wasmer.Value) ([]wasmer.Value, error) {
+			}
+
+			ptr := args[0].I32()
+			errStr := i.context.hostErr.Error()
+			data := i.mem.Data()
+			copy(data[ptr:], errStr)
+			return []wasmer.Value{}, nil
+		},
+	)
+	i.consoleLog = wasmer.NewFunction(
+		i.m.store,
+		wasmer.NewFunctionType(wasmer.NewValueTypes(wasmer.I32, wasmer.I32), wasmer.NewValueTypes()),
+		func(args []wasmer.Value) ([]wasmer.Value, error) {
+			if i.m.logger != nil {
+				data := i.mem.Data()
 				ptr := args[0].I32()
 				len := args[1].I32()
-				data := inst.mem.Data()
-				buf := make([]byte, len)
-				copy(buf, data[ptr:ptr+len])
-				inst.context.guestResp = buf
-				return []wasmer.Value{}, nil
-			},
-		),
-		"__guest_error": wasmer.NewFunction(
-			store,
-			wasmer.NewFunctionType(wasmer.NewValueTypes(wasmer.I32, wasmer.I32), wasmer.NewValueTypes()),
-			func(args []wasmer.Value) ([]wasmer.Value, error) {
-				ptr := args[0].I32()
-				len := args[1].I32()
-				data := inst.mem.Data()
-				cp := make([]byte, len)
-				copy(cp, data[ptr:ptr+len])
-				inst.context.guestErr = string(cp)
-				return []wasmer.Value{}, nil
-			},
-		),
-		"__host_call": wasmer.NewFunction(
-			store,
-			wasmer.NewFunctionType(wasmer.NewValueTypes(wasmer.I32, wasmer.I32, wasmer.I32, wasmer.I32, wasmer.I32, wasmer.I32, wasmer.I32, wasmer.I32), wasmer.NewValueTypes(wasmer.I32)),
-			func(args []wasmer.Value) ([]wasmer.Value, error) {
-				bindingPtr := args[0].I32()
-				bindingLen := args[1].I32()
-				namespacePtr := args[2].I32()
-				namespaceLen := args[3].I32()
-				operationPtr := args[4].I32()
-				operationLen := args[5].I32()
-				payloadPtr := args[6].I32()
-				payloadLen := args[7].I32()
-
-				if inst.m.hostCallHandler == nil {
-					return []wasmer.Value{wasmer.NewI32(0)}, nil
-				}
-
-				data := inst.mem.Data()
-				binding := string(data[bindingPtr : bindingPtr+bindingLen])
-				namespace := string(data[namespacePtr : namespacePtr+namespaceLen])
-				operation := string(data[operationPtr : operationPtr+operationLen])
-				payload := make([]byte, payloadLen)
-				copy(payload, data[payloadPtr:payloadPtr+payloadLen])
-
-				inst.context.hostResp, inst.context.hostErr = inst.m.hostCallHandler(inst.context.ctx, binding, namespace, operation, payload)
-				if inst.context.hostErr != nil {
-					return []wasmer.Value{wasmer.NewI32(0)}, nil
-				}
-
-				return []wasmer.Value{wasmer.NewI32(1)}, nil
-			},
-		),
-		"__host_response_len": wasmer.NewFunction(
-			store,
-			wasmer.NewFunctionType(wasmer.NewValueTypes(), wasmer.NewValueTypes(wasmer.I32)),
-			func(args []wasmer.Value) ([]wasmer.Value, error) {
-				return []wasmer.Value{wasmer.NewI32(int32(len(inst.context.hostResp)))}, nil
-			},
-		),
-		"__host_response": wasmer.NewFunction(
-			store,
-			wasmer.NewFunctionType(wasmer.NewValueTypes(wasmer.I32), wasmer.NewValueTypes()),
-			func(args []wasmer.Value) ([]wasmer.Value, error) {
-				if inst.context.hostResp != nil {
-					ptr := args[0].I32()
-					data := inst.mem.Data()
-					copy(data[ptr:], inst.context.hostResp)
-				}
-				return []wasmer.Value{}, nil
-			},
-		),
-		"__host_error_len": wasmer.NewFunction(
-			store,
-			wasmer.NewFunctionType(wasmer.NewValueTypes(), wasmer.NewValueTypes(wasmer.I32)),
-			func(args []wasmer.Value) ([]wasmer.Value, error) {
-				if inst.context.hostErr == nil {
-					return []wasmer.Value{wasmer.NewI32(0)}, nil
-				}
-				errStr := inst.context.hostErr.Error()
-				return []wasmer.Value{wasmer.NewI32(int32(len(errStr)))}, nil
-			},
-		),
-		"__host_error": wasmer.NewFunction(
-			store,
-			wasmer.NewFunctionType(wasmer.NewValueTypes(wasmer.I32), wasmer.NewValueTypes()),
-			func(args []wasmer.Value) ([]wasmer.Value, error) {
-				if inst.context.hostErr == nil {
-					return []wasmer.Value{}, nil
-				}
-
-				ptr := args[0].I32()
-				errStr := inst.context.hostErr.Error()
-				data := inst.mem.Data()
-				copy(data[ptr:], errStr)
-				return []wasmer.Value{}, nil
-			},
-		),
-		"__console_log": wasmer.NewFunction(
-			store,
-			wasmer.NewFunctionType(wasmer.NewValueTypes(wasmer.I32, wasmer.I32), wasmer.NewValueTypes()),
-			func(args []wasmer.Value) ([]wasmer.Value, error) {
-				if inst.m.logger != nil {
-					data := inst.mem.Data()
-					ptr := args[0].I32()
-					len := args[1].I32()
-					msg := string(data[ptr : ptr+len])
-					inst.m.logger(msg)
-				}
-				return []wasmer.Value{}, nil
-			},
-		),
-	}
-}
-
-func wasiRuntime(store *wasmer.Store, inst *Instance) map[string]wasmer.IntoExtern {
+				msg := string(data[ptr : ptr+len])
+				i.m.logger(msg)
+			}
+			return []wasmer.Value{}, nil
+		},
+	)
 	return map[string]wasmer.IntoExtern{
-		"fd_write": wasmer.NewFunction(
-			store,
-			wasmer.NewFunctionType(wasmer.NewValueTypes(wasmer.I32, wasmer.I32, wasmer.I32, wasmer.I32), wasmer.NewValueTypes(wasmer.I32)),
-			func(args []wasmer.Value) ([]wasmer.Value, error) {
-				fileDescriptor := args[0].I32()
-				iovsPtr := args[1].I32()
-				iovsLen := args[2].I32()
-				writtenPtr := args[3].I32()
-
-				// Only writing to standard out (1) is supported
-				if fileDescriptor != 1 {
-					return []wasmer.Value{wasmer.NewI32(0)}, nil
-				}
-
-				if inst.m.writer == nil {
-					return []wasmer.Value{wasmer.NewI32(0)}, nil
-				}
-				data := inst.mem.Data()
-				iov := data[iovsPtr:]
-				bytesWritten := uint32(0)
-
-				for iovsLen > 0 {
-					iovsLen--
-					base := binary.LittleEndian.Uint32(iov)
-					length := binary.LittleEndian.Uint32(iov[4:])
-					stringBytes := data[base : base+length]
-					inst.m.writer(string(stringBytes))
-					iov = iov[8:]
-					bytesWritten += length
-				}
-
-				binary.LittleEndian.PutUint32(data[writtenPtr:], bytesWritten)
-
-				return []wasmer.Value{wasmer.NewI32(int32(bytesWritten))}, nil
-			},
-		),
+		"__guest_request":     i.guestRequest,
+		"__guest_response":    i.guestResponse,
+		"__guest_error":       i.guestError,
+		"__host_call":         i.hostCall,
+		"__host_response_len": i.hostResponseLen,
+		"__host_response":     i.hostResponse,
+		"__host_error_len":    i.hostErrorLen,
+		"__host_error":        i.hostError,
+		"__console_log":       i.consoleLog,
 	}
 }
 
-// addFunctions adds external functions to a slice so that they are not garbage collected.
-func (i *Instance) addFunctions(f map[string]wasmer.IntoExtern) map[string]wasmer.IntoExtern {
-	for _, ie := range f {
-		i.functions = append(i.functions, ie)
+func (i *Instance) wasiRuntime() map[string]wasmer.IntoExtern {
+	i.fdWrite = wasmer.NewFunction(
+		i.m.store,
+		wasmer.NewFunctionType(wasmer.NewValueTypes(wasmer.I32, wasmer.I32, wasmer.I32, wasmer.I32), wasmer.NewValueTypes(wasmer.I32)),
+		func(args []wasmer.Value) ([]wasmer.Value, error) {
+			fileDescriptor := args[0].I32()
+			iovsPtr := args[1].I32()
+			iovsLen := args[2].I32()
+			writtenPtr := args[3].I32()
+
+			// Only writing to standard out (1) is supported
+			if fileDescriptor != 1 {
+				return []wasmer.Value{wasmer.NewI32(0)}, nil
+			}
+
+			if i.m.writer == nil {
+				return []wasmer.Value{wasmer.NewI32(0)}, nil
+			}
+			data := i.mem.Data()
+			iov := data[iovsPtr:]
+			bytesWritten := uint32(0)
+
+			for iovsLen > 0 {
+				iovsLen--
+				base := binary.LittleEndian.Uint32(iov)
+				length := binary.LittleEndian.Uint32(iov[4:])
+				stringBytes := data[base : base+length]
+				i.m.writer(string(stringBytes))
+				iov = iov[8:]
+				bytesWritten += length
+			}
+
+			binary.LittleEndian.PutUint32(data[writtenPtr:], bytesWritten)
+
+			return []wasmer.Value{wasmer.NewI32(int32(bytesWritten))}, nil
+		},
+	)
+
+	return map[string]wasmer.IntoExtern{
+		"fd_write": i.fdWrite,
 	}
-	return f
 }
 
 // MemorySize returns the memory length of the underlying instance.
@@ -366,17 +378,26 @@ func (i *Instance) Invoke(ctx context.Context, operation string, payload []byte)
 
 // Close closes the single instance.  This should be called before calling `Close` on the Module itself.
 func (i *Instance) Close() {
-	// Explicitly release handles on wasmer types so they can be GC'ed.
+	// Explicitly release references on wasmer types so they can be GC'ed.
 	i.inst = nil
 	i.mem = nil
-	i.functions = i._functions[:0]
-	i._functions = [initialNumFunctions]wasmer.IntoExtern{}
 	i.context = nil
+	i.guestRequest = nil
+	i.guestResponse = nil
+	i.guestError = nil
+	i.hostCall = nil
+	i.hostResponseLen = nil
+	i.hostResponse = nil
+	i.hostErrorLen = nil
+	i.hostError = nil
+	i.consoleLog = nil
+	i.abort = nil
+	i.fdWrite = nil
 }
 
 // Close closes the module.  This should be called after calling `Close` on any instances that were created.
 func (m *Module) Close() {
-	// Explicitly release handles on wasmer types so they can be GC'ed.
+	// Explicitly release references on wasmer types so they can be GC'ed.
 	m.module = nil
 	m.store = nil
 	m.engine = nil
